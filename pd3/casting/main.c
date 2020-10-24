d976dfb8e99b05f4d27dc25b3bf8d2f067372da7
Imants Pulkstenis <imants.pulkstenis@pm.me>
2020-10-24 16:17:00 +0300

    Praktiskie darbi 3

---
+++ pd3/casting/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdint.h>
+#define ARR_SIZE 32
+
+
+int main(){
+    uint8_t arr[4] = {0x01,0x02,0x03, 0x04};
+    
+    int32_t *ptr_int; //pointeris
+
+    float *ptr_float;
+
+    uint16_t val_uint16t;
+
+    // left: int32_t; rigt: uint8_t*
+    // ptr_int = arr;
+    ptr_int = (int32_t*)arr;  // ieraksta adresi arr[0]
+
+    printf("addr:%x HEX:0x%x DEC:%d\n", &ptr_float, *&*&*&*ptr_int, *ptr_int);
+    
+
+    // left: int32_t; rigt: uint8_t8
+    ptr_float = (float*)arr;  // ieraksta adresi arr[0]
+
+    printf("addr:%x HEX:0x%x DEC:%f\n", ptr_float, *ptr_float, *ptr_float);
+
+    // left: uint16_t; rigt: uint8_t*
+    val_uint16t = *(uint16_t*)arr;
+    printf("addr:%x HEX:0x%x DEC:%u\n", &val_uint16t, val_uint16t, val_uint16t);
+
+    return 0;
+}
---
+++ pd3/stack_reader/main.c
@@ -0,0 +1,29 @@
+// This is Stack reader
+#include <stdio.h>
+#include <stdint.h>
+#define ARR_SIZE 32
+
+void pollute(){
+    uint32_t arr[ARR_SIZE];
+    for (int i=0; i < ARR_SIZE; i++){
+        arr[i] = 2*i;
+    }
+}
+
+int main(void){
+    uint32_t arr[ARR_SIZE];     //defining array
+    printf("Array addres start: %p\n", &arr[0]);    // print start adres of stack
+    printf("Array addres stop: %p\n", &arr[ARR_SIZE -1]); 
+
+    for (int i=0; i < ARR_SIZE; i++){
+        pollute();  // izsaucam funkciju
+        arr[i] = arr[-i];
+        // arr[i]= *(arr-i);
+    }
+
+    for (int i=0; i < ARR_SIZE; i++){
+        printf("%u. %p : 0x%x (%u)\n",i, &arr[-i], arr[i], arr[i]);    
+    }
+ 
+    return 0;
+}
